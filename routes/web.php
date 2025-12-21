9d45fb61f265408c6503055acee6abcb5bf239ba
Dillan <114377647+ahmaddillan@users.noreply.github.com>
2025-12-21 13:50:24 +0700

    membuat homepage dari website

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+
+Route::get('/marketplace', function () {
+    return "Halaman Marketplace (Belum dibuat)";
+});
+
+Route::get('/aset', function () {
+    return "Halaman Aset Desa (Belum dibuat)";
+});
+
+Route::get('/pembangunan', function () {
+    return "Halaman Pembangunan (Belum dibuat)";
+});
+
+Route::get('/login', function () {
+    return "Halaman Login (Belum dibuat)";
 });
